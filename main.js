8e7f8ac3decc8419758692bf86639b0b7c804bc1
Lensman <anministry.mgmt@gmail.com>
2021-02-24 19:44:27 +0200

    Raw demo

--- main.js
+++ main.js
@@ -1,14 +1,66 @@
 let input = [
-  'Оцените пожалуйста сервис от 1(ого) до 10(ти). 10',
-  'Оцените пожалуйста сервис от 1(ого) до 10(ти). Десять.',
+  'Оцените пожалуйста сервис от 1(ого) до 10(ти). десять',
+  'Оцените пожалуйста .сер.ви.с о.т 1(ого) до 10(ти). десять.',
 ];
+
+let replaceDictionary = { '1(ого)': '>1', '10(ти)': '<10', десять: '10' };
 
 let convertedInput = [];
 let filteredInput = [];
+let finalResult = [];
+let tempArr = [];
 
 function convertInput(arr) {
-  convertInput = arr.split(' ');
-  return convertInput;
+  convertedInput = arr.replaceAll('.', '').split(' ');
+  return convertedInput;
 }
-// TODO: Add functionality to remove dots in line.
-console.log(convertInput(input[1]));
+
+function filterInput(arr) {
+  filteredInput = arr.map((v) => {
+    return replaceDictionary[v];
+  });
+  return filteredInput;
+}
+
+console.log(convertInput(input[0]));
+
+console.log(filterInput(convertInput(input[0])));
+
+console.log(filteredInput);
+
+console.log(convertedInput);
+
+// for (let i = 0; i < filteredInput.length; i++) {
+//   console.log(typeof filteredInput[i]);
+// }
+
+console.log(compare(filteredInput));
+
+console.log(input[0].indexOf(convertedInput[4]));
+
+console.log(filteredInput);
+// for (let i = 0; i < filteredInput.length; i++){
+//   console.log(input[0].indexOf(filteredInput[i]));
+// }
+
+function compare(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == 'undefined') {
+      continue;
+    } else {
+      tempArr.push(convertedInput[i]);
+    }
+  }
+}
+
+console.log(tempArr);
+
+for (let i = 0; i < tempArr.length; i++) {
+  console.log(input[0].indexOf(tempArr[i]));
+  let obj = {};
+  obj['text'] = tempArr[i];
+  obj['startPos'] = input[0].indexOf(tempArr[i]);
+  finalResult.push(obj);
+}
+
+console.log(finalResult);
